4405472b50a99a87104d5c1f019c78d556a1a162
Melvin Suratos <melvinsuratos@hotmail.com>
2021-06-30 22:12:31 -0700

    Base file structure and initial backend dotnet web api project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# astro-world
+# Astro World
 Web application that will allow end users to upload images and answer some trivia for my dog's, Astro Blue, first birthday party.
+
+## Technologies
+
+### Backend
+- <span>ASP.NET</span> 5
+- <span>ASP.NET</span> Web Api
+
+### Frontend
+- React
---
+++ src/backend/astro-world-api/Controllers/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace astro_world_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly ILogger<ImageController> _logger;
+
+        public ImageController(ILogger<ImageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("admin/images")]
+        public IActionResult GetAllImages() 
+        {
+            _logger.LogDebug("Admin is getting all the images");
+            return Ok("Got images");
+        }
+    }
+}
