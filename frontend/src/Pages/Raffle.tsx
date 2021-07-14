f5aa3190f957fc86f84f9bae2912bd400b637788
Melvin Suratos <melvinsuratos@hotmail.com>
2021-07-13 20:39:59 -0700

    Added the logic to call post http request to create user in frontend

--- frontend/src/Pages/Raffle.tsx
+++ frontend/src/Pages/Raffle.tsx
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import './Raffle.css';
 
 const Raffle = () => {
   const history = useHistory();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const cancel = () => history.push('/');
+  const createUser = async () => {
+    try {
+      const options:RequestInit = { 
+        method: 'POST', 
+        body: JSON.stringify({ FirstName: firstName, LastName: lastName })
+      };
+      const resp = await fetch(process.env.REACT_APP_API_URL + '/users', options);
+      if (!resp.ok) throw resp;
+  
+      const date = new Date();
+      date.setDate(date.getDate() + 1);
+  
+      document.cookie = `displayname=${firstName} ${lastName}; Path=/; Expires=${date}`;
+      setFirstName('');
+      setLastName('');
+    } catch (error) {
+      console.log('Error creating user', error);
+    }
+  };
 
   return (
     <div className="raffle">
       <h2 className="raffle-title">RAFFLE</h2>
       <h3>Enter Information</h3>
       <label htmlFor="firstname">First Name</label>
-      <input type="text" name="firstname" />
+      <input type="text" name="firstname" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
       <label htmlFor="lastname">Last Name</label>
-      <input type="text" name="lastname" />
+      <input type="text" name="lastname" onChange={(e) => setLastName(e.target.value)} value={lastName} />
       <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button className="btn btn__primary">Next</button>
+        <button className="btn btn__primary" onClick={createUser}>Next</button>
         <button className="btn btn__secondary" onClick={cancel}>Cancel</button>
       </div>
     </div>
