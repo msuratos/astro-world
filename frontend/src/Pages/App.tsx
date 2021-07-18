b71575eac314397b938163e1d61cf0af1a1d9dde
Melvin Suratos <melvinsuratos@hotmail.com>
2021-07-17 18:20:59 -0700

    Redesigned using ui-neumorphism library

--- frontend/src/Pages/App.tsx
+++ frontend/src/Pages/App.tsx
@@ -1,39 +1,46 @@
 import { useHistory } from "react-router-dom";
+import { 
+  Avatar, Button, CardHeader, CardContent, Divider
+} from "ui-neumorphism";
+
 import logo from '../assets/images/astro-blue.png';
 import './App.css';
 
 function App() {
   const history = useHistory();
-  const onButtonClick = (event: any) => {
-    const value = event.target.value;
+  const onButtonClick: Function = (event: any) => {
+    const value = event;
     switch (value) {
       case 'anonymous':
         history.push('/anonymous');
         break;
       case 'raffle':
         history.push('/raffle');
-        break;    
+        break;
       default:
         console.log(`Button with value (${value}) has been clicked`);
         break;
     }
   };
 
+  const avatar = (<Avatar src={logo} size={125} bgColor='var(--white)' className="avatar-custom" />);
+
   return (
-    <div className="App">
-      <div className="App-logo" >
-        <img src={logo} alt="Astro Pic" title="Astro Pic" />
-      </div>
-      <section className="App-details">
+    <>
+      <CardHeader avatar={avatar} className="card-header-custom" rounded />
+      <CardContent>
         <div className="title">
-          <section className="title-name">ASTRO BLUE</section>
-          <section className="title-desc">TURNS ONE!</section>
+          <label className="title-name">ASTRO BLUE</label>
+          <label className="title-desc">TURNS ONE!</label>
         </div>
-        <label><b>Upload Pics</b></label>
-        <button className="btn btn__secondary" value="anonymous" onClick={onButtonClick}>Anonymously</button>
-        <button className="btn btn__secondary" value="raffle" onClick={onButtonClick}>For Raffle</button>
-      </section>
-    </div>
+        <Divider dense />
+        <label className="app-label-custom"><b>Upload Pics</b></label>
+        <div className="btn-div">
+          <Button rounded color='var(--greyDark)' className="btn-custom" onClick={() => onButtonClick('anonymous')}>Anonymously</Button>
+          <Button rounded color='var(--greyDark)' className="btn-custom" onClick={() => onButtonClick('raffle')}>For Raffle</Button>
+        </div>
+      </CardContent>
+    </>
   );
 }
 
--- frontend/src/components/ImageUpload.tsx
+++ frontend/src/components/ImageUpload.tsx
@@ -1,4 +1,6 @@
 import React, { FormEvent, useContext, useEffect, useRef, useState } from 'react';
+import { Button } from "ui-neumorphism";
+
 import { uploadImage } from '../api/imageApi';
 import RootContext from '../RootContext';
 import { getCookie } from "../utils/cookieHelper";
@@ -41,7 +43,7 @@ const ImageUpload = (props:ImageUploadProps) => {
       <form encType="multipart/form-data" method="post" onSubmit={uploadImageFormSubmit}>
         <label htmlFor="images">Upload Images</label>
         <input ref={imageInputRef} type="file" name="images" title="File Upload" multiple />
-        <button className="btn btn__primary" style={{width:'100%', height:'3.0rem'}}>Upload</button>
+        <Button color='var(--light-bg-light-shadow)' bgColor='var(--primary)' style={{width:'100%', height:'3.0rem', marginTop: '1rem'}}>Upload</Button>
         <div style={{marginTop:'15px', color: "red"}}>
           <output ref={resultRef}></output>
         </div>
