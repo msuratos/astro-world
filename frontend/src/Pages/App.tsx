098065045db1a35f872aae87b20963db0f4a6574
Melvin Suratos <melvinsuratos@hotmail.com>
2021-07-19 13:07:17 -0700

    Added button links for trivia & scavenger hunt. Put the CardHeader in index

--- frontend/src/Pages/App.tsx
+++ frontend/src/Pages/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
-import { 
-  Avatar, Button, CardHeader, CardContent, Divider
+import {
+  Button
+  // Avatar, Button
+  //, CardHeader, CardContent, Divider
 } from "ui-neumorphism";
 
-import logo from '../assets/images/astro-blue.png';
+// import logo from '../assets/images/astro-blue.png';
 import './App.css';
 
 function App() {
   const history = useHistory();
+  const [showUploadSection, setShowUploadSection] = useState(false);
+
+  const onTriviaClick: Function = (event: any) => window.open('https://forms.gle/E75ttV9TkV9HiXt68');
+  const onHuntClick: Function = (event: any) => console.log('Opening Scavenger Hunt link');
+  const onUploadClick: Function = (event: any) => setShowUploadSection(true);
+
   const onButtonClick: Function = (event: any) => {
     const value = event;
     switch (value) {
@@ -23,23 +32,37 @@ function App() {
     }
   };
 
-  const avatar = (<Avatar src={logo} style={{width: 'auto'}} size={125} bgColor='var(--white)' className="avatar-custom" />);
+  // const avatar = (<Avatar src={logo} style={{ width: 'auto' }} size={125} bgColor='var(--white)' className="avatar-custom" />);
 
   return (
     <>
-      <CardHeader avatar={avatar} className="card-header-custom" rounded />
+      {/* <CardHeader avatar={avatar} className="card-header-custom" rounded />
       <CardContent>
         <div className="title">
           <label className="title-name">ASTRO BLUE</label>
           <label className="title-desc">TURNS ONE!</label>
         </div>
-        <Divider dense />
-        <label className="app-label-custom"><b>Upload Pics</b></label>
-        <div className="btn-div">
-          <Button rounded color='var(--greyDark)' className="btn-custom" onClick={() => onButtonClick('anonymous')}>Anonymously</Button>
-          <Button rounded color='var(--greyDark)' className="btn-custom" onClick={() => onButtonClick('raffle')}>For Raffle</Button>
-        </div>
-      </CardContent>
+        <Divider dense /> */}
+        {
+          !showUploadSection &&
+          <>
+            <Button rounded color='var(--greyDark)' className="btn-custom" onClick={onTriviaClick}>Trivia</Button>
+            <Button rounded color='var(--greyDark)' className="btn-custom" onClick={onHuntClick}>Scavenger Hunt</Button>
+            <Button rounded color='var(--greyDark)' className="btn-custom" onClick={onUploadClick}>Upload Pics</Button>
+          </>
+        }
+        {
+          showUploadSection &&
+          <>
+            <label className="app-label-custom"><b>Upload Pics</b></label>
+            <div className="btn-div">
+              <Button rounded className="btn-custom" onClick={() => onButtonClick('anonymous')}>Anonymously</Button>
+              <Button rounded className="btn-custom" onClick={() => onButtonClick('raffle')}>For Raffle</Button>
+            </div>
+            <Button rounded color='var(--greyDark)' onClick={() => setShowUploadSection(false)}>◀Back</Button>
+          </>
+        }
+      {/* </CardContent> */}
     </>
   );
 }
